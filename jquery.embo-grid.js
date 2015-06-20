0d0b5a7f96ea9c257ece4ac70f867f6ec7ffb021
Muyeen Ansar <muyeen@live.com>
2015-06-20 22:38:23 +0530

    Updated
    Works fine

--- jquery.embo-grid.js
+++ jquery.embo-grid.js
@@ -1,82 +1,126 @@
-( function($) {
+(function($) {
 	var gridRowDivTemplate = 
 	'<div id="{{RowId}}" class="{{CssClass}}">' +
 	'</div>';
 	
 	var defaultTemplate = 
-	'<div>' +
+	'<div id="{{Id}}">' +
 	'<h1>{{Title}}</h1>' +
 	'<div>{{Image}}</div>' +
 	'<p>{{Description}}</p>' +
 	'<div>{{Button}}</div>' +
 	'</div>';
 	
+	
 	var defaultGridRowData = {
 					Id : null,
 					Title: "",
 					Image: {
 						Url: "",
-						Height: "100px",
+						alt:"",
+						title: "",
+						Height: "",
 						Width: ""
 					},
 					Description: "",
 					CssClass: "",
-					Button: [{
+					Button: {
+						Template : "{{1}}",
+						1:{
+						Template: "",
 						Text: "",
 						Url: "",
-						CssClass = ""
-					}]
-				}
-				
-	$.fn.embo = function(options){
-		var settings = $.extend({
-			template = defaultTemplate,
-			data: null,
-			complete: completed,
-			fail: failed,
-			end: ended
-		}, options);
-		return this.each(function(){
-			try{
-				var container = this;
-				
-				if (settings.data.rows)
-				{
-					var gridData = settings.data.rows;
-					gridData.each{
-						var rowData = $.extend(defaultGridRowData, this);
-						var row = applyRowDataToTemplate(settings.template, data);
-						$(container).append(row);
+						CssClass : ""
 					}
-				}
-				
-				if ($.isFunction(settings.complete)) {
-					settings.complete.call();
-				}
-			}
-			catch(ex)
-			{
-				if ($.isFunction(settings.fail)) {
-					settings.fail.call(ex);
-				}
-			}
-			finally {
-			  if ($.isFunction(settings.end)) {
-					settings.end.call();
-				}
-			}
-		});
+					}
+				};
+		
+	var EmboCore = {
+		Messages: {
+			error : "Something went wrong :"
+		},
+		completed: function (){
+			
+		},
+		failed: function (ex) {
+			EmboCore.showMessage('error', EmboCore.Messages.error + ex);
+		},
+		ended : function () {
+			
+		},
+		showMessage : function (type, message){
+			console.log(message);
+		}
 		
 	}
 	
-	var applyRowDataToTemplate = function (template, data){}
-	
-	var completed = function() {}
-	
-	var failed = function(ex) {
-		console.log("Something went wrong: " + ex);
+	var EmboGridCore = {
+		drawGrid : function (gridContainer, template, data) {
+			$.each(data, function(key, rowdata) {
+				$(gridContainer).append(
+				EmboGridCore.repeater(template, rowdata)
+				);
+				
+			});
+		},
+		repeater: function (template, data) {
+			
+			var row = template;
+			
+			$.each(data, function (key, value)
+			{
+				var keyString = '{{'+ key +'}}';
+				
+				if (typeof value == 'object') {
+					var objectTemplate = "";
+					
+					if (value.Template){
+						objectTemplate = value.Template;
+					}
+					row = row.replace(keyString, EmboGridCore.repeater(objectTemplate, value));
+					
+				} else {
+					row = row.replace(keyString, value);
+				}
+			});
+			EmboCore.showMessage(row);
+			return row;
+		}
 	}
 	
-	var ended = function() {}
+    $.fn.emboGrid = function(options) {
+		var settings = $.extend({
+			   template : defaultTemplate,
+			   data: null, 
+			   completed: EmboCore.completed,
+			   failed: EmboCore.failed,
+			   ended: EmboCore.ended
+		   }, options);
+		   console.log(settings.data);
+		return this.each(function(){
+			
+				var gridContainer = this;
+				
+				EmboGridCore.drawGrid(gridContainer, settings.template, settings.data);
+				if ($.isFunction(settings.completed)) {
+					settings.completed.call(this);
+				}
+			
+			try{}
+			catch(ex)
+			{
+				if ($.isFunction(settings.failed)) {
+					settings.failed.call(ex);
+				}
+			}
+			finally {
+			  if ($.isFunction(settings.ended)) {
+					settings.ended.call(this);
+				}
+			}
+			
+		});
+		
+    }
 	
 }(jQuery));
